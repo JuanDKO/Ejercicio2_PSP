50ef6400dbf7f8607f2f759c1ef5f2162605e623
JuanDKO <juandvidrodriguezlara2006@gmail.com>
2025-09-30 14:37:05 +0200

    Cambios significativos en la forma del codigo

---
+++ src/Contar.java
@@ -0,0 +1,126 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+    public class Contar extends Thread {
+        private String letra;
+
+        private String lectura;
+
+        private String escritura;
+
+        public Contar(String letra, String leer, String escribir) {
+            this.letra = letra;
+            this.lectura = leer;
+            this.escritura = escribir;
+        }
+
+        public int contarLetra() {
+            int contador = 0;
+            try(BufferedReader in = new BufferedReader(new FileReader(lectura))) {
+                int caracterleido;
+                while ((caracterleido = in.read()) != -1) {
+                    char let = (char) caracterleido;
+                    String letraaComprobar = String.valueOf(let);
+
+                    switch (letra.toLowerCase()) {
+                        case "a":
+                            if (comprobarTildeA(letraaComprobar)) {
+                                contador++;
+                            }
+                            break;
+                        case "e":
+                            if (comprobarTildeE(letraaComprobar)) {
+                                contador++;
+                            }
+                            break;
+                        case "i":
+                            if (comprobarTildeI(letraaComprobar)) {
+                                contador++;
+                            }
+                            break;
+                        case "o":
+                            if (comprobarTildeO(letraaComprobar)) {
+                                contador++;
+                            }
+                            break;
+                        case "u":
+                            if (comprobarTildeU(letraaComprobar)) {
+                                contador++;
+                            }
+                            break;
+                    }
+                }
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+
+            return contador;
+        }
+        public void subirDatos() {
+            try(BufferedWriter out = new BufferedWriter(new FileWriter(escritura))) {
+                out.write(contarLetra());
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        public boolean comprobarTildeA(String letra) {
+            if (letra.equalsIgnoreCase("a") && letra.equalsIgnoreCase("á")) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+        public boolean comprobarTildeE(String letra) {
+            if (letra.equalsIgnoreCase("e") && letra.equalsIgnoreCase("é")) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+        public boolean comprobarTildeI(String letra) {
+            if (letra.equalsIgnoreCase("i") && letra.equalsIgnoreCase("í")) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+        public boolean comprobarTildeO(String letra) {
+            if (letra.equalsIgnoreCase("o") && letra.equalsIgnoreCase("ó")) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+        public boolean comprobarTildeU(String letra) {
+            if (letra.equalsIgnoreCase("u") && letra.equalsIgnoreCase("ú")) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+
+
+        @Override
+        public void run() {
+            subirDatos();
+        }
+
+        public void secuencial_run() {
+            for (int i = 1; i <= 5; i++) {
+                try {
+                    subirDatos();
+                    Thread.sleep(1000); // Simula trabajo de 1 segundo
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(" terminó.");
+        }
+    }
+
--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,113 +1,25 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 
-public class Ejercicio1 {
+public class Ejercicio1 extends Thread {
 
-    private class contar {
-        private String letra;
-
-        private String lectura;
-
-        private String Escritura;
-
-        public contar(String letra, String leer, String escribir) {
-            this.letra = letra;
-            this.lectura = leer;
-            this.Escritura = escribir;
-        }
-
-        public int contarLetra() {
-            int contador = 0;
-            try(BufferedReader in = new BufferedReader(new FileReader(lectura))) {
-                int caracterleido;
-                while ((caracterleido = in.read()) != -1) {
-                    char let = (char) caracterleido;
-                    String letraaComprobar = String.valueOf(let);
-
-                    switch (letraaComprobar.toLowerCase()) {
-                        case "a":
-                            if (comprobarTildeA(letraaComprobar)) {
-                                contador++;
-                            }
-                            break;
-                        case "e":
-                            if (comprobarTildeE(letraaComprobar)) {
-                                contador++;
-                            }
-                            break;
-                        case "i":
-                            if (comprobarTildeI(letraaComprobar)) {
-                                contador++;
-                            }
-                            break;
-                        case "o":
-                            if (comprobarTildeO(letraaComprobar)) {
-                                contador++;
-                            }
-                            break;
-                        case "u":
-                            if (comprobarTildeU(letraaComprobar)) {
-                                contador++;
-                            }
-                            break;
-                    }
-                }
-            }
-            catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-
-
-            return contador;
-        }
-
-        public boolean comprobarTildeA(String letra) {
-            if (letra.equalsIgnoreCase("a") && letra.equalsIgnoreCase("á")) {
-                return true;
-            }else {
-                return false;
-            }
-        }
-        public boolean comprobarTildeE(String letra) {
-            if (letra.equalsIgnoreCase("e") && letra.equalsIgnoreCase("é")) {
-                return true;
-            }else {
-                return false;
-            }
-        }
-        public boolean comprobarTildeI(String letra) {
-            if (letra.equalsIgnoreCase("i") && letra.equalsIgnoreCase("í")) {
-                return true;
-            }else {
-                return false;
-            }
-        }
-        public boolean comprobarTildeO(String letra) {
-            if (letra.equalsIgnoreCase("o") && letra.equalsIgnoreCase("ó")) {
-                return true;
-            }else {
-                return false;
-            }
-        }
-        public boolean comprobarTildeU(String letra) {
-            if (letra.equalsIgnoreCase("u") && letra.equalsIgnoreCase("ú")) {
-                return true;
-            }else {
-                return false;
-            }
-        }
-
-
-
-
-    }
 
     public static void main(String[] args) {
+        final String LEER = "src/leer.txt";
         final String LECTURA_A ="src/CuentaA.txt";
         final String LECTURA_E ="src/CuentaE.txt";
         final String LECTURA_I ="src/CuentaI.txt";
         final String LECTURA_O ="src/CuentaO.txt";
         final String LECTURA_U ="src/CuentaU.txt";
+
+         new Contar("a",LEER,LECTURA_A).secuencial_run();
+         new Contar("e",LEER,LECTURA_E).secuencial_run();
+         new Contar("i",LEER,LECTURA_I).secuencial_run();
+         new Contar("o",LEER,LECTURA_O).secuencial_run();
+         new Contar("u",LEER,LECTURA_U).secuencial_run();
+
 
 
     }
